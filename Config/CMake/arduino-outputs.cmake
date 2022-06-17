8945e8832d84bab16bb08f0b39ccf6db20e20ee2
Olivier Saudan <osa@secolive.ch>
2022-06-17 22:51:45 +0200

    CMake config: automatically include startup code on flash images

--- Config/CMake/arduino-outputs.cmake
+++ Config/CMake/arduino-outputs.cmake
@@ -1,5 +1,7 @@
 function(buildAsFlashImage target)
   set_target_properties(${target} PROPERTIES SUFFIX ".elf")
+  target_sources(${target} PRIVATE ${ARDUINO_STARTUP_SOURCE})
+
   setOptionsForFlashImage(${target})
 endfunction()
 
