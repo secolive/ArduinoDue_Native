183af5da771cfb10f452656bca39ec9fe7877923
Olivier Saudan <osa@secolive.ch>
2022-07-03 20:33:39 +0200

    CMake Config: do not apply target compile options when building on host

--- Config/CMake/BuildOptionsStandard.cmake
+++ Config/CMake/BuildOptionsStandard.cmake
@@ -1,18 +1,19 @@
-#Uncomment for hardware floating point
-#add_compile_definitions(ARM_MATH_CM4;ARM_MATH_MATRIX_CHECK;ARM_MATH_ROUNDING)
-#add_compile_options(-mfloat-abi=hard -mfpu=fpv4-sp-d16)
-#add_link_options(-mfloat-abi=hard -mfpu=fpv4-sp-d16)
+if(NOT DEFINED ARDUINO_BUILD_FOR_HOST)
+  #Uncomment for hardware floating point
+  #add_compile_definitions(ARM_MATH_CM4;ARM_MATH_MATRIX_CHECK;ARM_MATH_ROUNDING)
+  #add_compile_options(-mfloat-abi=hard -mfpu=fpv4-sp-d16)
+  #add_link_options(-mfloat-abi=hard -mfpu=fpv4-sp-d16)
 
-#Uncomment for software floating point
-add_compile_options(-mfloat-abi=soft)
+  #Uncomment for software floating point
+  add_compile_options(-mfloat-abi=soft)
 
-# uncomment to mitigate c++17 absolute addresses warnings
-#set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-register")
+  # uncomment to mitigate c++17 absolute addresses warnings
+  #set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-register")
 
-add_compile_options(--param max-inline-insns-single=500)
-add_compile_options(-fno-exceptions)
+  add_compile_options(--param max-inline-insns-single=500)
+  add_compile_options(-fno-exceptions)
 
-# These apply only to C++
-add_compile_options($<$<COMPILE_LANGUAGE:CXX>:-fno-threadsafe-statics>)
-add_compile_options($<$<COMPILE_LANGUAGE:CXX>:-fno-rtti>)
-
+  # These apply only to C++
+  add_compile_options($<$<COMPILE_LANGUAGE:CXX>:-fno-threadsafe-statics>)
+  add_compile_options($<$<COMPILE_LANGUAGE:CXX>:-fno-rtti>)
+endif ()
